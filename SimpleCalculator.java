4dd76dcdf5aa4508e2bdf76d211986bfe8fd9e05
ashishpareek28 <pareekashish1214@gmail.com>
2025-01-21 12:52:14 +0530

    Add files via upload

---
+++ SimpleCalculator.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Simple Calculator!");
+
+        while (true) {
+            System.out.println("\nSelect an operation:");
+            System.out.println("1. Add");
+            System.out.println("2. Subtract");
+            System.out.println("3. Multiply");
+            System.out.println("4. Divide");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice (1/2/3/4/5): ");
+            int choice = scanner.nextInt();
+
+            if (choice == 5) {
+                System.out.println("Exiting calculator. Goodbye!");
+                break;
+            }
+
+            System.out.print("Enter first number: ");
+            double num1 = scanner.nextDouble();
+            System.out.print("Enter second number: ");
+            double num2 = scanner.nextDouble();
+
+            double result;
+
+            switch (choice) {
+                case 1:
+                    result = num1 + num2;
+                    System.out.println("Result: " + num1 + " + " + num2 + " = " + result);
+                    break;
+
+                case 2:
+                    result = num1 - num2;
+                    System.out.println("Result: " + num1 + " - " + num2 + " = " + result);
+                    break;
+
+                case 3:
+                    result = num1 * num2;
+                    System.out.println("Result: " + num1 + " * " + num2 + " = " + result);
+                    break;
+
+                case 4:
+                    if (num2 == 0) {
+                        System.out.println("Error! Division by zero is not allowed.");
+                    } else {
+                        result = num1 / num2;
+                        System.out.println("Result: " + num1 + " / " + num2 + " = " + result);
+                    }
+                    break;
+
+                default:
+                    System.out.println("Invalid choice! Please select a valid operation.");
+            }
+        }
+
+        scanner.close();
+    }
+}
